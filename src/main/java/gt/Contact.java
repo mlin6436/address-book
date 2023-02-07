f1c7c51c006bbf5a7f72f25060283a6bb1d2d617
Meng Lin <meng.lin@slalom.com>
2023-02-07 19:27:43 +0000

    Add contact object

---
+++ src/main/java/gt/Contact.java
@@ -0,0 +1,7 @@
+package gt;
+
+import java.time.LocalDate;
+
+public record Contact (String name, Gender gender, LocalDate dateOfBirth) {
+    
+}
---
+++ src/main/java/gt/Gender.java
@@ -0,0 +1,5 @@
+package gt;
+public enum Gender {
+    Female,
+    Male
+}
